2443cd352b6579788e83a57d794ec2449f59ce61
philba <p.bayer@freemail.hu>
2017-04-09 01:40:44 +0200

    Temperature interpolation impl

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,11 +1,29 @@
 package observatory
 
+import java.lang.Math.pow
+
 import com.sksamuel.scrimage.Image
+import org.apache.spark.sql.functions.sum
+import org.apache.spark.sql.{Row, SparkSession}
+
+import scala.math._
 
 /**
 	* 2nd milestone: basic visualization
 	*/
 object Visualization {
+
+	val R = 6372.8 //radius in km
+	val p = 2
+	private val spark: SparkSession =
+		SparkSession
+			.builder()
+			.appName("Time Usage")
+			.config("spark.master", "local[4]")
+			.getOrCreate()
+
+	import spark.implicits._
+	//		import spark.implicits._
 
 	/**
 		* @param temperatures Known temperatures: pairs containing a location and the temperature at this location
@@ -13,7 +31,33 @@ object Visualization {
 		* @return The predicted temperature at `location`
 		*/
 	def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-		???
+		temperatures.find(temp => temp._1 == location)
+			.map(_._2)
+			.getOrElse({
+				spark.sparkContext.parallelize(temperatures.toSeq)
+					.toDF("location", "temperature")
+					.map(r => {
+						val temp = r.getAs[Double]("temperature")
+						val locRow = r.getAs[Row]("location")
+						val distance = approximateDistance(locRow.getAs[Double]("lat"), locRow.getAs[Double]("lon"), location)
+						val invWeight = 1 / pow(distance, p)
+						(temp * invWeight, invWeight)
+					})
+					.withColumnRenamed("_1", "weightedTemp")
+					.withColumnRenamed("_2", "weight")
+					.select(sum($"weightedTemp").as("weightedTemp"), sum($"weight")).as("weight")
+					.select($"weightedTemp".divide($"weight").as[Double])
+					.first()
+			})
+	}
+
+	protected[observatory] def approximateDistance(lat: Double, lon: Double, location2: Location): Double = {
+		val dLat = (lat - location2.lat).toRadians
+		val dLon = (lon - location2.lon).toRadians
+
+		val a = pow(sin(dLat / 2), 2) + pow(sin(dLon / 2), 2) * cos(lat.toRadians) * cos(location2.lat.toRadians)
+		val c = 2 * asin(sqrt(a))
+		R * c
 	}
 
 	/**
@@ -22,6 +66,7 @@ object Visualization {
 		* @return The color that corresponds to `value`, according to the color scale defined by `points`
 		*/
 	def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
+
 		???
 	}
 
--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -9,5 +9,15 @@ import org.scalatest.prop.Checkers
 @RunWith(classOf[JUnitRunner])
 class VisualizationTest extends FunSuite with Checkers {
 
+	test("predictTemperature") {
+		val datapoints = Seq(
+			(Location(1, 2), 3D),
+			(Location(2, 2), 4D),
+			(Location(1, 3), 6D),
+			(Location(2, 1), 1D)
+		)
+		val result = Visualization.predictTemperature(datapoints, Location(2, 3))
+		println(result)
+	}
 
 }
