85d07fe49ac48cbc390b92f1989b92d43ce931b7
philba <p.bayer@freemail.hu>
2017-04-12 21:29:48 +0200

    object for profiling

---
+++ src/main/scala/observatory/util/Profiler.scala
@@ -0,0 +1,27 @@
+package observatory.util
+
+import org.slf4j.LoggerFactory
+import org.slf4j.event.Level
+
+/**
+	* Created by philba on 4/12/17.
+	*/
+object Profiler {
+	val logger = LoggerFactory.getLogger(Profiler.getClass)
+
+
+	def runProfiled[T](msg: String, level: Level = Level.INFO)(action: => T) = {
+		val t1 = System.nanoTime
+		val t = action
+		val duration = (System.nanoTime - t1) / 1e9d
+		level match {
+			case Level.INFO => logger.info(msg + " took {} seconds.", duration)
+			case Level.DEBUG => logger.debug(msg + " took {} seconds.", duration)
+			case Level.TRACE => logger.trace(msg + " took {} seconds.", duration)
+			case Level.WARN => logger.warn(msg + " took {} seconds.", duration)
+			case Level.ERROR => logger.error(msg + " took {} seconds.", duration)
+		}
+		t
+	}
+
+}
