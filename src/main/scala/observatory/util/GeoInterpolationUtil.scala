f921e98eb9d8d39694f0c4cba178cc099be50dec
philba <p.bayer@freemail.hu>
2017-04-27 21:08:40 +0200

    Test for distance

--- src/main/scala/observatory/util/GeoInterpolationUtil.scala
+++ src/main/scala/observatory/util/GeoInterpolationUtil.scala
@@ -3,7 +3,6 @@ package observatory.util
 import java.lang.Math.pow
 
 import observatory.Location
-import observatory.Visualization.R
 
 import scala.math._
 
@@ -12,8 +11,9 @@ import scala.math._
 	*/
 object GeoInterpolationUtil {
 
+	private val R = 6372.8 //radius in km
 
-	protected[observatory] def approximateDistance(lat: Double, lon: Double, location2: Location): Double = {
+	def approximateDistance(lat: Double, lon: Double, location2: Location): Double = {
 		val dLat = (lat - location2.lat).toRadians
 		val dLon = (lon - location2.lon).toRadians
 
@@ -22,16 +22,16 @@ object GeoInterpolationUtil {
 		R * c
 	}
 
-	protected[observatory] def approximateDistance(location1: Location, location2: Location): Double = {
+	def approximateDistance(location1: Location, location2: Location): Double = {
 		val dLat = (location1.lat - location2.lat).toRadians
-		val dLon = (location1.lat - location2.lon).toRadians
+		val dLon = (location1.lon - location2.lon).toRadians
 
 		val a = pow(sin(dLat / 2), 2) + pow(sin(dLon / 2), 2) * cos(location1.lat.toRadians) * cos(location2.lat.toRadians)
 		val c = 2 * asin(sqrt(a))
 		R * c
 	}
 
-	protected[observatory] def approximateDistance(location1: OptimizedLocation, location2: Location): Double = {
+	def approximateDistance(location1: OptimizedLocation, location2: Location): Double = {
 		val lat1Rad = location2.lat.toRadians
 		val dLat = location1.latRad - lat1Rad
 		val dLon = location1.lonRad - location2.lon.toRadians
@@ -47,20 +47,10 @@ object GeoInterpolationUtil {
 		val cosLat = cos(latRad)
 	}
 
-	//	protected[observatory] def approximateDistanceOpti(location1: Location, location2: Location): Double = {
-	//
-	//
-	//		val a = location1.latSinus * location2.latSinus
-	//		val b = location1.latCosinus * location2.latCosinus * cos(location2.lonRadians - location1.lonRadians)
-	//		val c = acos(a + b)
-	//		//		val c = Optimizer.acos(a + b)
-	//		R * c
-	//	}
-
 	type LatSinusCosinusLonRadians = (Double, Double, Double)
 
-	protected[observatory] def approximateDistanceOpti(location1: LatSinusCosinusLonRadians,
-																										 location2: Location): Double = {
+	def approximateDistanceOpti(location1: LatSinusCosinusLonRadians,
+															location2: Location): Double = {
 		val lat2 = location2.lat.toRadians
 		val lon2 = location2.lon.toRadians
 
@@ -71,7 +61,7 @@ object GeoInterpolationUtil {
 		R * c
 	}
 
-	protected[observatory] def approximateDistanceOpti(lat: Double, lon: Double, location2: Location): Double = {
+	def approximateDistanceOpti(lat: Double, lon: Double, location2: Location): Double = {
 		val lat1 = lat.toRadians
 		val lat2 = location2.lat.toRadians
 		val lon1 = lon.toRadians
