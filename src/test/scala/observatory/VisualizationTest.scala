a05b337efbb1dd01c909cde2ca0b62d9d9bf9474
philba <p.bayer@freemail.hu>
2017-04-29 19:06:58 +0200

    More tests

--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -10,6 +10,17 @@ import org.scalatest.{FunSuite, Matchers}
 @RunWith(classOf[JUnitRunner])
 class VisualizationTest extends FunSuite with Matchers with TableDrivenPropertyChecks {
 
+	val grads = Seq(
+		(60D, Color(255, 255, 255)),
+		(32D, Color(255, 0, 0)),
+		(12D, Color(255, 255, 0)),
+		(0D, Color(0, 255, 255)),
+		(-15D, Color(0, 0, 255)),
+		(-27D, Color(255, 0, 255)),
+		(-50D, Color(33, 0, 107)),
+		(-60D, Color(0, 0, 0))
+	)
+
 	test("predictTemperature") {
 		val datapoints = Seq(
 			(Location(1, 2), 3D),
@@ -22,33 +33,31 @@ class VisualizationTest extends FunSuite with Matchers with TableDrivenPropertyC
 	}
 
 	test("predictTemperature real dataset") {
-		val records = Extraction
-			.locationYearlyAverageRecords(Extraction.locateTemperatures(1975, "/stations.csv", "/1975.csv"))
+		val data = TestDataUtil.fetchTestDataForYear(1975)
 
-		val t1 = System.nanoTime
-		val result = Visualization.predictTemperature(records, Location(2, 3))
-		val duration = (System.nanoTime - t1) / 1e9d
-		println(duration)
+		val result = Visualization.predictTemperature(data, Location(2, 3))
 		println(result)
 	}
 
-	test("visualize real dataset") {
-		val grads = Seq(
-			(60D, Color(255, 255, 255)),
-			(32D, Color(255, 0, 0)),
-			(12D, Color(255, 255, 0)),
-			(0D, Color(0, 255, 255)),
-			(-15D, Color(0, 0, 255)),
-			(-27D, Color(255, 0, 255)),
-			(-50D, Color(33, 0, 107)),
-			(-60D, Color(0, 0, 0))
-		)
+	test("predictTemperature fake dataset") {
+		val data = List((Location(45.0, -90.0), -41.195131039498634), (Location(-45.0, 0.0), 1.0))
+		val img = Visualization.visualize(data, grads)
+		val pixel = img.pixel(90, 45)
+		println(pixel.toColor)
+	}
 
+	test("color for fake dataset") {
+		//		val data = List((Location(45.0,-90.0),-41.195131039498634), (Location(-45.0,0.0),1.0))
+		val data = List((Location(45.0, -90.0), 61.09273383703933), (Location(-45.0, 0.0), 0.0))
+		val temp = Visualization.predictTemperature(data, Location(45, -90))
+	}
+
+	test("visualize real dataset") {
 		val year = 1975
-    val data = TestDataUtil.fetchTestDataForYear(year)
+		val data = TestDataUtil.fetchTestDataForYear(year)
 
 		val t1 = System.nanoTime
-    val result = Visualization.visualize(data, grads, 1)
+		val result = Visualization.visualize(data, grads, 1)
 		val duration = (System.nanoTime - t1) / 1e9d
 		result.output(s"vizualImg$year.png")
 		println(duration)
