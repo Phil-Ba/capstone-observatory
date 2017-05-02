e5a37903bb1762c9c07410e9e63d97e0cb86ca13
philba <p.bayer@freemail.hu>
2017-05-02 21:52:55 +0200

    Method for seq

--- src/main/scala/observatory/util/ConversionUtil.scala
+++ src/main/scala/observatory/util/ConversionUtil.scala
@@ -1,17 +1,22 @@
 package observatory.util
 
 import observatory.Location
+import observatory.models.OPixel
 
 /**
-  * Created by pbayer on 25/04/2017.
-  */
+	* Created by pbayer on 25/04/2017.
+	*/
 class ConversionUtil(val baseWidth: Int = 360, val baseHeight: Int = 180) {
 
-  private val startLon = -baseWidth / 2
-  private val startLat = baseHeight / 2
+	private val startLon = -baseWidth / 2
+	private val startLat = baseHeight / 2
 
-  def pixelToGps(x: Int, y: Int, scale: Int = 1) = {
-    Location(startLat - y / scale, x / scale + startLon)
-  }
+	def pixelToGps(x: Int, y: Int, scale: Int = 1): Location = {
+		Location(startLat - y / scale, x / scale + startLon)
+	}
+
+	def pixelsToGps(xys: Seq[OPixel], scale: Int = 1): Seq[(OPixel, Location)] = {
+		xys.map(xy => (xy, pixelToGps(xy._1, xy._2, scale)))
+	}
 
 }
