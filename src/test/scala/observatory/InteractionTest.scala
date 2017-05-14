ec773246b150122cdfa69588bb0f2f00a84311bd
philba <p.bayer@freemail.hu>
2017-05-14 22:26:34 +0200

    whole range of years

--- src/test/scala/observatory/InteractionTest.scala
+++ src/test/scala/observatory/InteractionTest.scala
@@ -1,6 +1,6 @@
 package observatory
 
-import observatory.util.{SlipperyMap, TestDataUtil}
+import observatory.util.TestDataUtil
 import org.scalatest.FunSuite
 import org.scalatest.prop.Checkers
 
@@ -8,38 +8,36 @@ import scala.reflect.io.Path
 
 class InteractionTest extends FunSuite with Checkers {
 
-	test("GenerateTiles from real dataset") {
+  test("GenerateTiles from real dataset") {
 
-		def imgFunction(year: Int, zoom: Int, x: Int, y: Int, data: Iterable[(Location, Double)]) = {
-			val tileImage = Interaction.tile(data, Main.grads, zoom, x, y)
-			Path(s"src/generated/resources/temperatures/$year/$zoom").createDirectory()
-			tileImage.output(s"src/generated/resources/temperatures/$year/$zoom/$x-$y.png")
-		}
+    def imgFunction(year: Int, zoom: Int, x: Int, y: Int, data: Iterable[(Location, Double)]) = {
+      val image = s"src/generated/resources/temperatures/$year/$zoom/$x-$y.png"
+      if (!Path(image).exists) {
+        val tileImage = Interaction.tile(data, Main.grads, zoom, x, y)
+        Path(s"src/generated/resources/temperatures/$year/$zoom").createDirectory()
+        tileImage.output(image)
+      }
+    }
 
-		val data: Seq[(Location, Double)] = TestDataUtil.fetchTestDataForYear(1975)
-		val yearlyData = Seq((1975, data))
-		Interaction.generateTiles(yearlyData, imgFunction, 3)
-	}
+    for (yearToVisualize <- 1975 to 2015) {
+      val dataForYear = TestDataUtil.fetchTestDataForYear(yearToVisualize)
+      val yearlyData = Seq((yearToVisualize, dataForYear))
+      Interaction.generateTiles(yearlyData, imgFunction, 3)
+    }
 
-	test("GenerateTile from real dataset") {
+  }
 
-		def imgFunction(year: Int, zoom: Int, x: Int, y: Int, data: Iterable[(Location, Double)]) = {
-			val tileImage = Interaction.tile(data, Main.grads, zoom, x, y)
-			Path(s"src/generated/resources/temperatures/$year/$zoom").createDirectory()
-			tileImage.output(s"src/generated/resources/temperatures/$year/$zoom/$x-$y.png")
-		}
+  test("GenerateTile from real dataset") {
 
-		val data: Seq[(Location, Double)] = TestDataUtil.fetchTestDataForYear(1975)
-		val yearlyData = Seq((1975, data))
-		Interaction.generateTile(yearlyData, imgFunction, 1)
-	}
+    def imgFunction(year: Int, zoom: Int, x: Int, y: Int, data: Iterable[(Location, Double)]) = {
+      val tileImage = Interaction.tile(data, Main.grads, zoom, x, y)
+      Path(s"src/generated/resources/temperatures/$year/$zoom").createDirectory()
+      tileImage.output(s"src/generated/resources/temperatures/$year/$zoom/$x-$y.png")
+    }
 
-	test("yxc") {
-		val temperatures = Seq((Location(45.0, -90.0), 20.0), (Location(45.0, 90.0), 0.0), (Location(0.0, 0.0), 10.0),
-			(Location(-45.0, -90.0), 0.0), (Location(-45.0, 90.0), 20.0))
-		val testLocation = SlipperyMap.LatLonPoint(84.92832092949963, -180.0, 0).toTile
-
-		val tile = Interaction.tile(temperatures, Main.grads, 0, 0, 0)
-	}
+    val data: Seq[(Location, Double)] = TestDataUtil.fetchTestDataForYear(1975)
+    val yearlyData = Seq((1975, data))
+    Interaction.generateTile(yearlyData, imgFunction, 1)
+  }
 
 }
