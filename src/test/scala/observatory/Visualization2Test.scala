1ed30824bbbfa0824b29312af0468833282a980a
philba <p.bayer@freemail.hu>
2017-05-14 21:56:44 +0200

    additional years added

--- src/test/scala/observatory/Visualization2Test.scala
+++ src/test/scala/observatory/Visualization2Test.scala
@@ -11,32 +11,34 @@ import scala.reflect.io.Path
 @RunWith(classOf[JUnitRunner])
 class Visualization2Test extends FunSuite with Checkers {
 
-	test("generate dev image") {
-		val yearToVisualize = 1975
-		val yearStart = 1975
-		val yearEnd = 1989
+  test("generate dev image") {
+    val yearStart = 1975
+    val yearEnd = 1989
 
-		val normals: Seq[Seq[(Location, Double)]] = for {
-			year <- yearStart to yearEnd
-		} yield {
-			TestDataUtil.fetchTestDataForYear(year)
-		}
+    val normals: Seq[Seq[(Location, Double)]] = for {
+      year <- yearStart to yearEnd
+    } yield {
+      TestDataUtil.fetchTestDataForYear(year)
+    }
 
-		val dataForYear = TestDataUtil.fetchTestDataForYear(yearToVisualize)
-		val averageGrid = Manipulation.average(normals)
-		val deviationGrid = Manipulation.deviation(dataForYear, averageGrid)
+    val averageGrid = Manipulation.average(normals)
 
-		val zooms = TilesUtil.xyValuesForZooms(0, 1, 2, 3)
-		for (xyz <- zooms) {
-			Profiler.runProfiled(s"generating deviation(zoom:${xyz._3} | x=${xyz._1}, y=${xyz._2})") {
-				val image = s"src/generated/resources/deviations/$yearToVisualize/${xyz._3}/${xyz._1}-${xyz._2}.png"
-				if (!Path(image).exists) {
-					val tileImage = Visualization2.visualizeGridMonix(deviationGrid, Main.gradsDeviation, xyz._3, xyz._1, xyz._2)
-					Path(s"src/generated/resources/deviations/$yearToVisualize/${xyz._3}").createDirectory()
-					tileImage.output(image)
-				}
-			}
-		}
-	}
+    for (yearToVisualize <- 1975 to 2000) {
+      val dataForYear = TestDataUtil.fetchTestDataForYear(yearToVisualize)
+      val deviationGrid = Manipulation.deviation(dataForYear, averageGrid)
+
+      val zooms = TilesUtil.xyValuesForZooms(0, 1, 2, 3)
+      for (xyz <- zooms) {
+        Profiler.runProfiled(s"generating deviation(zoom:${xyz._3} | x=${xyz._1}, y=${xyz._2})") {
+          val image = s"src/generated/resources/deviations/$yearToVisualize/${xyz._3}/${xyz._1}-${xyz._2}.png"
+          if (!Path(image).exists) {
+            val tileImage = Visualization2.visualizeGridMonix(deviationGrid, Main.gradsDeviation, xyz._3, xyz._1, xyz._2)
+            Path(s"src/generated/resources/deviations/$yearToVisualize/${xyz._3}").createDirectory()
+            tileImage.output(image)
+          }
+        }
+      }
+    }
+  }
 
 }
